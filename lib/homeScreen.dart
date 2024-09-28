5bdf00b5be8369ed92c9bf47941d36fb9ecfe0b2
Ahmed0100078 <your-email@example.com>
2024-09-28 18:29:37 +0300

    try push

---
+++ lib/homeScreen.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double sliderVal =100;
+
+  double weight = 0.0;
+  int? age;
+
+  bool b = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+        title: Text('BMI Calculator'),
+
+        actions: [
+          Icon(Icons.dark_mode),
+          Icon(Icons.accessibility_new_sharp),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          if(!b){
+
+                            b = !b;
+                          }
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: b? Colors.blue : Colors.grey[300],
+                        ),
+                        child: Column(
+                          children: [
+                            Icon(Icons.male),
+                            Text('male')
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8,),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          if(b){
+                            b = !b;
+                          }
+                        });
+
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: b? Colors.grey[300] : Colors.blue,
+                        ),
+                        child: Column(
+                          children: [
+                            Icon(Icons.female),
+                            Text('Female')
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8,),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300]
+                ),
+                child: Column(
+                  children: [
+                    Text('Height',
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w800
+                      ),),
+                    Text('${sliderVal.toInt()}cm',
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w800
+                      ),),
+                    Slider(value: sliderVal, onChanged: (val){
+                      setState(() {
+                        sliderVal = val;
+                        print(val.toInt());
+                      });
+
+                    },
+                      min: 100,
+                      max: 210,),
+
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 8,),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(/// Elevated button , textButton , floating action button
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                      ),
+                      child: Column(
+                        children: [
+                          Text('Age',
+                            style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.w800
+                            ),),
+                          Text('${age}',
+                            style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.w800
+                            ),),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(onPressed: (){
+                                setState(() {
+                                  age = (age ?? 0) + 1;
+                                });
+                              }, icon: Icon(Icons.add)),
+                              IconButton(onPressed: (){
+                                setState(() {
+                                  if (age! > 0){
+                                    age = (age ?? 0) - 1;
+                                  }else {print('can not do this op');}
+
+                                });
+                              }, icon: Icon(Icons.minimize_sharp))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8,),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                      ),
+                      child: Column(
+                        children: [
+                          Text('Weight',
+                            style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.w800
+                            ),),
+                          Text('${weight}',
+                            style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.w800
+                            ),),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(onPressed: (){
+                                setState(() {
+                                  weight ++;
+                                });
+                              }, icon: Icon(Icons.add)),
+                              IconButton(onPressed: (){
+                                setState(() {
+                                  if (weight > 0){
+                                    weight--;
+                                  }else{}
+
+                                });
+                              }, icon: Icon(Icons.minimize_rounded))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),   /// Column , Row , Container , Stack , SafeArea , Lists , SizedBox , PlaceHolder
+    );
+  }
+}
---
+++ lib/messengerScreen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class Messengerscreen extends StatelessWidget {
+  const Messengerscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: const CircleAvatar(
+            backgroundColor: Colors.red,
+          ),
+        ),
+        title: const Text('Chats',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 25,
+        ),),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.camera_alt_rounded),),
+    IconButton(onPressed: (){}, icon: Icon(Icons.add_card_rounded),),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search_off_outlined),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.grey.withOpacity(0.1))),
+
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.grey.withOpacity(0.1))),
+                  filled: true,
+                  fillColor: Colors.grey[250],
+
+                ),
+              ),
+              SizedBox(height: 8,),
+              Container(
+                height: 70,
+                child: Container(
+                  height: 70,
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index)=> buildStoryItem(),
+                      separatorBuilder: (context, index)=>SizedBox(width: 8,),
+                      itemCount: 40),
+                ),
+              ),
+              SizedBox(height: 8,),
+              ListView.separated(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) => buildChatItem(),
+                  separatorBuilder: (context, index) => SizedBox(height: 8,),
+                  itemCount: 10)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildStoryItem() => Row(
+  children: [
+    Stack(
+      alignment: AlignmentDirectional.bottomEnd,
+      children: [
+        CircleAvatar(
+
+          radius: 20,
+          backgroundImage: AssetImage('assets/img1.jpg'),
+        ),
+        CircleAvatar(
+          radius: 5,
+          backgroundColor: Colors.green ,
+        )
+      ],
+    ),
+  ],
+);
+
+Widget buildChatItem() => Container(
+  height: 80,
+  child: Row(
+    children: [
+      CircleAvatar(radius: 100,backgroundImage: AssetImage('assets/img1.jpg'),),
+      Column(
+        children: [
+          Text('Ahmed MO'),
+          Text('Hi I am there'),
+        ],
+      ),
+      SizedBox(width: 655,),
+      Icon(Icons.check_outlined)
+    ],
+  ),
+);
